b7cf0161343cd63d325be5f9ca4aaf75ff67c424
Murat Aybey Nurçin <49798089+AybeyNurcin@users.noreply.github.com>
2026-07-20 14:42:01 +0300

    dosyalar güncellendi

--- Database/tablo_olusturma/A_create_table.sql
+++ Database/tablo_olusturma/A_create_table.sql
@@ -1,27 +1,25 @@
 CREATE TABLE KB_MUSTERIBILGILERI(
-    ID                   NUMBER(19)                                  NOT NULL,
-    AD                   VARCHAR2(50)                                NOT NULL,
-    SOYAD                VARCHAR2(50)                                NOT NULL,
-    EPOSTA               VARCHAR2(50) UNIQUE                         NOT NULL,
-    SIFRE                VARCHAR2 (20)                               NOT NULL,
-    DOGUMTARIHI          DATE DEFAULT SYSDATE                        NOT NULL,
-    TELEFONNO            VARCHAR2(13)                                NOT NULL,
-    TCKN                 NUMBER(11) UNIQUE                           NOT NULL, 
-    CINSIYET             NUMBER(1) CHECK (CINSIYET IN (1,2))         NOT NULL,
-    VKN                  NUMBER(10) UNIQUE                           NOT NULL,
-    MUSTERITIPI          NUMBER(1) CHECK (MUSTERITIPI IN (1,2))      NOT NULL,
-    SUBESUBEKODU         VARCHAR2(20)                                NOT NULL,
-    DURUMKODU            NUMBER(1) CHECK (DURUMKODU IN (1,2))        NOT NULL,
-    UNVAN                VARCHAR2(50)                                NOT NULL,
-    KAYITOLUSTURMATARIHI DATE                                        NOT NULL,
-    RECORDUSER           VARCHAR2(10),                                
-    RECORDDATE           DATE DEFAULT SYSDATE,
-
-    CONSTRAINT PK_KB_MUSTERIBILGILERI PRIMARY KEY (ID)
+    ID                      NUMBER(19)          DEFAULT ON NULL KB_MUSTERIBILGILERI_SEQ.NEXTVAL PRIMARY KEY,
+    AD                      VARCHAR2(50)                                    NOT NULL,
+    SOYAD                   VARCHAR2(50)                                    NOT NULL,
+    EPOSTA                  VARCHAR2(50)        UNIQUE                      NOT NULL,
+    SIFRE                   VARCHAR2 (20)                                   NOT NULL,
+    DOGUMTARIHI             DATE                DEFAULT SYSDATE             NOT NULL,
+    TELEFONNO               VARCHAR2(13)                                    NOT NULL,
+    TCKN                    NUMBER(11)          UNIQUE                      NOT NULL, 
+    CINSIYET                NUMBER(1)           CHECK (CINSIYET IN (1,2))   NOT NULL,
+    VKN                     NUMBER(10)          UNIQUE                      NOT NULL,
+    MUSTERITIPI             NUMBER(1)           CHECK(MUSTERITIPI IN (1,2)) NOT NULL,
+    SUBESUBEKODU            VARCHAR2(20)                                    NOT NULL,
+    DURUMKODU               NUMBER(1)           CHECK (DURUMKODU IN (1,2))  NOT NULL,
+    UNVAN                   VARCHAR2(50)                                    NOT NULL,
+    KAYITOLUSTURMATARIHI    DATE                                            NOT NULL,
+    RECORDUSER              VARCHAR2(10),                                
+    RECORDDATE              DATE                DEFAULT SYSDATE
 );
 
 create TABLE KB_MUSTERIILETISIM(
-    ID NUMBER(19) PRIMARY KEY,
+    ID NUMBER(19) DEFAULT ON NULL KB_MUSTERIILETISIM_SEQ.NEXTVAL PRIMARY KEY,
     ILETISIMTIPI NUMBER(1) CHECK (ILETISIMTIPI IN (1, 2, 3, 4, 5, 6)) NOT NULL,
     ILETISIM VARCHAR2(100) NOT NULL,
     MUSTERIBILGILERIID NUMBER NOT NULL,
@@ -30,5 +28,4 @@ create TABLE KB_MUSTERIILETISIM(
     RECORDDATE DATE DEFAULT SYSDATE
 );
 
-alter table KB_MUSTERIILETISIM modify ID NUMBER(19);
-alter table KB_MUSTERIILETISIM modify MUSTERIBILGILERIID NUMBER(19);
+commit;
