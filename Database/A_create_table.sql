34970885dc6423ecdba358fc678121ef8c505e69
Ahmet <ahmet.tbk.2000@gmail.com>
2026-07-20 15:25:06 +0300

    Insertler atıldı.

---
+++ Database/A_create_table.sql
@@ -0,0 +1,33 @@
+CREATE TABLE KB_MUSTERIBILGILERI(
+    ID                   NUMBER(19)                                  NOT NULL,
+    AD                   VARCHAR2(50)                                NOT NULL,
+    SOYAD                VARCHAR2(50)                                NOT NULL,
+    EPOSTA               VARCHAR2(50) UNIQUE                         NOT NULL,
+    SIFRE                VARCHAR2 (20)                               NOT NULL,
+    DOGUMTARIHI          DATE DEFAULT SYSDATE                        NOT NULL,
+    TELEFONNO            VARCHAR2(13)                                NOT NULL,
+    TCKN                 NUMBER(11) UNIQUE                           NOT NULL, 
+    CINSIYET             NUMBER(1) CHECK (CINSIYET IN (1,2))         NOT NULL,
+    VKN                  NUMBER(10) UNIQUE                           NOT NULL,
+    MUSTERITIPI          NUMBER(1) CHECK (MUSTERITIPI IN (1,2))      NOT NULL,
+    SUBESUBEKODU         VARCHAR2(20)                                NOT NULL,
+    DURUMKODU            NUMBER(1) CHECK (DURUMKODU IN (1,2))        NOT NULL,
+    UNVAN                VARCHAR2(50)                                NOT NULL,
+    KAYITOLUSTURMATARIHI DATE                                        NOT NULL,
+    RECORDUSER           VARCHAR2(10),                                
+    RECORDDATE           DATE DEFAULT SYSDATE,
+
+    CONSTRAINT PK_KB_MUSTERIBILGILERI PRIMARY KEY (ID)
+);
+
+create TABLE KB_MUSTERIILETISIM(
+    ID NUMBER(19) PRIMARY KEY,
+    ILETISIMTIPI NUMBER(1) CHECK (ILETISIMTIPI IN (1, 2, 3, 4, 5, 6)) NOT NULL,
+    ILETISIM VARCHAR2(100) NOT NULL,
+    MUSTERIBILGILERIID NUMBER NOT NULL,
+    CONSTRAINT FK_KB_MUSTERIILETISIM_MUSTERIBILGILERIID FOREIGN KEY (MUSTERIBILGILERIID) REFERENCES KB_MUSTERIBILGILERI(ID) ON DELETE CASCADE,
+    RECORDUSER VARCHAR2(10),
+    RECORDDATE DATE DEFAULT SYSDATE
+);
+
+alter table KB_MUSTERIILETISIM modify ID NUMBER(19);
