56c5358a6893c8a52e1cb70ac0db51783c9db2e2
Murat Aybey Nurçin <49798089+AybeyNurcin@users.noreply.github.com>
2026-07-22 14:17:28 +0300

    DTO değişkenleri güncellendi

--- Backend/kocerbank-backend/Models/DTOs/MusteriDTO.cs
+++ Backend/kocerbank-backend/Models/DTOs/MusteriDTO.cs
@@ -1,4 +1,5 @@
 namespace kocerbank_backend.Models.DTOs;
+
 public class MusteriDTO
 {
     public long Id { get; set; }
@@ -14,6 +15,7 @@ public class MusteriDTO
     public byte MusteriTipi { get; set; }
     public string SubeKodu { get; set; }
     public byte DurumKodu { get; set; }
+    public string Unvan { get; set; }
     public DateTime KayitOlusturmaTarihi { get; set; }
     public string RecordUser { get; set; }  
     public DateTime RecordDate { get; set; }
