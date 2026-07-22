182f3e66e109c3102ad5f6f9e134b6bbe7b1034c
Metehan Unal <unallmetehan@gmail.com>
2026-07-22 15:30:13 +0300

    .

--- Backend/kocerbank-backend/Models/DTOs/SubeDTO.cs
+++ Backend/kocerbank-backend/Models/DTOs/SubeDTO.cs
@@ -1,20 +1,20 @@
-namespace kocerbank_backend.Models.DTOs;
+namespace kocerbank_backend.Models.DTOs{
+    public class SubeDTO
+    {
+        public long Id { get; set; }
 
-public class SubeDTO
-{
-    public long Id { get; set; }
+        public string SubeAdi { get; set; } = string.Empty;
 
-    public string SubeAdi { get; set; } = string.Empty;
+        public string SubeKodu { get; set; } = string.Empty;
 
-    public string SubeKodu { get; set; } = string.Empty;
+        public string SubeTelefonNo { get; set; } = string.Empty;
 
-    public string SubeTelefonNo { get; set; } = string.Empty;
+        public string SubeAdres { get; set; } = string.Empty;
 
-    public string SubeAdres { get; set; } = string.Empty;
+        public byte SubeDurumKodu { get; set; }
 
-    public byte SubeDurumKodu { get; set; }
+        public string RecordUser { get; set; } = string.Empty;
 
-    public string RecordUser { get; set; } = string.Empty;
-
-    public DateTime RecordDate { get; set; }
+        public DateTime RecordDate { get; set; }
+    }
 }
