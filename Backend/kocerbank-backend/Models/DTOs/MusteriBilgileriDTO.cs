097a77001d806db62749223eba2ba1f3c9ebd785
Murat Aybey Nurçin <49798089+AybeyNurcin@users.noreply.github.com>
2026-07-22 10:42:58 +0300

    Müşteri Bilgiler DTO dosyası oluşturuldu

---
+++ Backend/kocerbank-backend/Models/DTOs/MusteriBilgileriDTO.cs
@@ -0,0 +1,20 @@
+namespace kocerbank_backend.Models.DTOs;
+public class MusteriBilgileriDTO
+{
+    public long Id { get; set; }
+    public string Ad { get; set; } 
+    public string Soyad { get; set; }
+    public string Eposta { get; set; }
+    public string Sifre { get; set; }
+    public DateTime DogumTarihi { get; set; }
+    public string TelefonNo { get; set; }
+    public int TCKN { get; set; }
+    public byte Cinsiyet{ get; set; }
+    public int VKN { get; set; }
+    public byte MusteriTipi { get; set; }
+    public string SubeKodu { get; set; }
+    public byte DurumKodu { get; set; }
+    public DateTime KayitOlusturmaTarihi { get; set; }
+    public string RecordUser { get; set; }  
+    public DateTime RecordDate { get; set; }
+}
