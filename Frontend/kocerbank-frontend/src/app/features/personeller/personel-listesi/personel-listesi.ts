088cf17fe8501b117db68e18ae8825490509de27
Metehan Unal <unallmetehan@gmail.com>
2026-07-27 15:26:38 +0300

    Html routelarına başlandı.

---
+++ Frontend/kocerbank-frontend/src/app/features/personeller/personel-listesi/personel-listesi.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personel-listesi',
+  standalone: false,
+  templateUrl: './personel-listesi.html',
+  styleUrl: './personel-listesi.css',
+})
+export class PersonelListesi {
+
+}
