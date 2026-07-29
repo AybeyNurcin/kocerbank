a23069d59b116f37bf606885d938c266aa49602e
Ahmet <ahmet.tbk.2000@gmail.com>
2026-07-29 17:43:24 +0300

    .....

--- Database/tablo_doldurma/M_insert.sql
+++ Database/tablo_doldurma/M_insert.sql
@@ -3,7 +3,14 @@ INSERT INTO KB_SUBE (SUBEADI, SUBETELEFONNO, SUBEADRES, SUBEDURUMKODU) VALUES ('
 COMMIT;
 
 INSERT INTO KB_PERSONEL (AD, SOYAD, ROL, SIFRE, TCKN, TELEFONNO, ADRES, EPOSTA, SUBESUBEKODU, DURUMKODU) VALUES ('Metehan', 'Ünal', 'Admin', 'admin123', 12345678901, '05551234567', 'İstanbul, Türkiye', 'metehan.unal@kocerbank.com', 'S0008', 1);
-INSERT INTO KB_PERSONEL (AD, SOYAD, ROL, SIFRE, TCKN, TELEFONNO, ADRES, EPOSTA, SUBESUBEKODU, DURUMKODU) VALUES ('Ahmet', '7abak', 'Admin', 'admin123', 12345678902, '05551234568', 'İstanbul, Türkiye', 'ahmet.7abak@kocerbank.com', 'S0009', 1);
+INSERT INTO KB_PERSONEL (AD, SOYAD, ROL, SIFRE, TCKN, TELEFONNO, ADRES, EPOSTA, SUBESUBEKODU, DURUMKODU) VALUES ('Ahmet', '7abak', 'Admin', 'admin123', 12345678902, '05551234568', 'İstanbul, Türkiye', 'ahmet.7abak@kocerbank.com', 'S0034', 1);
+INSERT INTO KB_PERSONEL (AD, SOYAD, ROL, SIFRE, TCKN, TELEFONNO, ADRES, EPOSTA, SUBESUBEKODU, DURUMKODU) VALUES ('Ayşegül', 'Kara', 'Admin', 'admin123', 12345678903, '05551234569', 'İstanbul, Türkiye', 'aysegul.kara@kocerbank.com', 'S0035', 1);
+insert into KB_PERSONEL (AD, SOYAD, ROL, SIFRE, TCKN, TELEFONNO, ADRES, EPOSTA, SUBESUBEKODU, DURUMKODU) values ('Ayşegül', 'Kara', 'Admin', 'admin123', 12345678903, '05551234569', 'İstanbul, Türkiye', 'aysegul.kara@kocerbank.com', 'S0035', 1);
+insert into KB_PERSONEL (AD, SOYAD, ROL, SIFRE, TCKN, TELEFONNO, ADRES, EPOSTA, SUBESUBEKODU, DURUMKODU) values ('Ayşe', 'Kar', 'Admin', 'admin123', 12345678905, '05551234560', 'İstanbul, Türkiye', 'ayşe.kar@kocerbank.com', 'S0037', 1);
+
+
+
+
 
 
 UPDATE KB_PERSONEL SET AD = 'Metehan' WHERE ID = 3;
